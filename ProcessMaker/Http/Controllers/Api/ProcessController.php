6e2f25518ec84f4d980f7b4793622f2e016c8185
Agustin Busso <agustin.busso@processmaker.com>
2023-03-15 12:30:05 -0300

    Fix rate endpoint

--- ProcessMaker/Http/Controllers/Api/ProcessController.php
+++ ProcessMaker/Http/Controllers/Api/ProcessController.php
@@ -1215,12 +1215,19 @@ class ProcessController extends Controller
 
         $config = $aiProcessHandler->getConfig();
         $model = $aiProcessHandler->getModel();
+        $industry = $request->input('industry');
         $ratings = $request->input('ratings');
-        $hash = md5($model . $config);
+
+        $configForHash = &$config;
+        unset($configForHash['prompt']);
+        unset($configForHash['question']);
+
+        $hash = md5($model . $configForHash);
 
         foreach ($ratings as $rating) {
             AIModelRating::create([
                 'hash' => $hash,
+                'industry' => $industry,
                 'model' => $model,
                 'config' => $config,
                 'rating' => $rating->rating,
