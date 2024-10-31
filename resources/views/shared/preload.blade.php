a1d0ef8ba9111dbc6b2d64c6923da0f65942258f
David Callizaya <davidcallizaya@gmail.com>
2024-10-30 20:49:31 -0400

    Include app2.js into preload

--- resources/views/shared/preload.blade.php
+++ resources/views/shared/preload.blade.php
@@ -1,4 +1,5 @@
 <!-- Preload JS -->
+<link rel="preload" href="{{ mix('js/app2.js') }}" as="script">
 <link rel="preload" href="{{ mix('js/app.js') }}" as="script">
 <link rel="preload" href="{{ mix('js/vendor.js') }}" as="script">
 <link rel="preload" href="/vendor/monaco-editor/min/vs/editor/editor.main.js" as="script">
