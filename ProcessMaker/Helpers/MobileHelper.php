d86a1d8a3e44b958c2b30bbe930fb39ecafb9d32
David Callizaya <davidcallizaya@gmail.com>
2024-10-30 20:00:14 -0400

    Use mobile first approach

--- ProcessMaker/Helpers/MobileHelper.php
+++ ProcessMaker/Helpers/MobileHelper.php
@@ -6,6 +6,9 @@ class MobileHelper
 {
     public static function isMobile($userAgent)
     {
+        // @todo changed to use mobile first approach
+        return false;
+
         $device = '/(Mobile|Android|Tablet|GoBrowser|[0-9]x[0-9]*|uZardWeb\/|Mini|Doris\/|Skyfire\/|iPhone|Fennec\/|Maemo|Iris\/|CLDC\-|Mobi\/)/uis';
         if (preg_match($device, $userAgent) && !empty($_COOKIE['isMobile']) && $_COOKIE['isMobile'] === 'true') {
             return true;
